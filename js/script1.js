a49925b107a428b8b2e139d2e34eb1315eeb6045
Nathan Webb <nwrocketman@byui.edu>
2021-03-10 09:36:01 -0700

    Added Week 10 Notes

--- js/script1.js
+++ js/script1.js
@@ -54,6 +54,12 @@ window.addEventListener('load', (event)=>{
                 label: "Week 9 Notes",
                 url: "week9/index.html"
             }
+        ],
+        [
+            {
+                label: "Week 10 Notes",
+                url: "week10/index.html"
+            }
         ]
     ];
 
